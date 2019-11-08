fc8801f26a04768660369c32d2387987b13c5220
Trey <tajohns22@gmail.com>
2019-11-08 15:08:57 -0600

    Added header component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from "./components/header/header.component";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header></Header>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, eligendi. Tempora excepturi totam labore ab. Asperiores deleniti, eligendi itaque impedit cupiditate molestias perferendis harum eaque sit illum iusto animi architecto?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, cumque ipsam reprehenderit consequatur nulla, impedit cum corrupti cupiditate libero dicta neque. Rerum quas labore dignissimos vel aliquid, velit dolorum earum!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, fugit id? Corporis ullam quidem numquam maxime eos quae tempore beatae qui. Quaerat nisi eius quo fugiat, sint possimus necessitatibus fuga.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia minus nesciunt omnis excepturi ullam, dicta facilis doloribus, reprehenderit impedit molestiae doloremque, velit earum vel ab animi illum dolores blanditiis? Eum?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, animi sapiente. Ipsa praesentium cumque quisquam, vero minima quam consectetur provident iure dolorum corrupti nobis repudiandae in dolores sit itaque aperiam!
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit assumenda nisi modi dolor cum molestias itaque hic. Commodi voluptate, esse fuga tenetur excepturi voluptatum, itaque, eaque architecto maxime nulla alias!
+      </h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, eligendi. Tempora excepturi totam labore ab. Asperiores deleniti, eligendi itaque impedit cupiditate molestias perferendis harum eaque sit illum iusto animi architecto?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, cumque ipsam reprehenderit consequatur nulla, impedit cum corrupti cupiditate libero dicta neque. Rerum quas labore dignissimos vel aliquid, velit dolorum earum!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, fugit id? Corporis ullam quidem numquam maxime eos quae tempore beatae qui. Quaerat nisi eius quo fugiat, sint possimus necessitatibus fuga.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia minus nesciunt omnis excepturi ullam, dicta facilis doloribus, reprehenderit impedit molestiae doloremque, velit earum vel ab animi illum dolores blanditiis? Eum?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, animi sapiente. Ipsa praesentium cumque quisquam, vero minima quam consectetur provident iure dolorum corrupti nobis repudiandae in dolores sit itaque aperiam!
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit assumenda nisi modi dolor cum molestias itaque hic. Commodi voluptate, esse fuga tenetur excepturi voluptatum, itaque, eaque architecto maxime nulla alias!
+      </h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, eligendi. Tempora excepturi totam labore ab. Asperiores deleniti, eligendi itaque impedit cupiditate molestias perferendis harum eaque sit illum iusto animi architecto?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, cumque ipsam reprehenderit consequatur nulla, impedit cum corrupti cupiditate libero dicta neque. Rerum quas labore dignissimos vel aliquid, velit dolorum earum!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, fugit id? Corporis ullam quidem numquam maxime eos quae tempore beatae qui. Quaerat nisi eius quo fugiat, sint possimus necessitatibus fuga.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia minus nesciunt omnis excepturi ullam, dicta facilis doloribus, reprehenderit impedit molestiae doloremque, velit earum vel ab animi illum dolores blanditiis? Eum?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, animi sapiente. Ipsa praesentium cumque quisquam, vero minima quam consectetur provident iure dolorum corrupti nobis repudiandae in dolores sit itaque aperiam!
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit assumenda nisi modi dolor cum molestias itaque hic. Commodi voluptate, esse fuga tenetur excepturi voluptatum, itaque, eaque architecto maxime nulla alias!
+      </h1>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, eligendi. Tempora excepturi totam labore ab. Asperiores deleniti, eligendi itaque impedit cupiditate molestias perferendis harum eaque sit illum iusto animi architecto?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, cumque ipsam reprehenderit consequatur nulla, impedit cum corrupti cupiditate libero dicta neque. Rerum quas labore dignissimos vel aliquid, velit dolorum earum!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, fugit id? Corporis ullam quidem numquam maxime eos quae tempore beatae qui. Quaerat nisi eius quo fugiat, sint possimus necessitatibus fuga.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia minus nesciunt omnis excepturi ullam, dicta facilis doloribus, reprehenderit impedit molestiae doloremque, velit earum vel ab animi illum dolores blanditiis? Eum?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, animi sapiente. Ipsa praesentium cumque quisquam, vero minima quam consectetur provident iure dolorum corrupti nobis repudiandae in dolores sit itaque aperiam!
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit assumenda nisi modi dolor cum molestias itaque hic. Commodi voluptate, esse fuga tenetur excepturi voluptatum, itaque, eaque architecto maxime nulla alias!
+      </h1>
     </div>
   );
 }
